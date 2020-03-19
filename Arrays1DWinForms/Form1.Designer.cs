0b1e02b6604cd2b1f6fa902fa1b183b11616f798
Oleh-UmerT <52933972+Oleh-UmerT@users.noreply.github.com>
2020-03-19 11:24:55 +0200

    Add files via upload

---
+++ Arrays1DWinForms/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Arrays1DWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridViewTable = new System.Windows.Forms.DataGridView();
+            this.numericUpDownCount = new System.Windows.Forms.NumericUpDown();
+            this.labelSum = new System.Windows.Forms.Label();
+            this.dataGridViewSort = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(422, 257);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Нечухраний О.І. ІПЗ-19-1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sum";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 31);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sort";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridViewTable
+            // 
+            this.dataGridViewTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTable.Location = new System.Drawing.Point(11, 50);
+            this.dataGridViewTable.Name = "dataGridViewTable";
+            this.dataGridViewTable.Size = new System.Drawing.Size(480, 68);
+            this.dataGridViewTable.TabIndex = 5;
+            // 
+            // numericUpDownCount
+            // 
+            this.numericUpDownCount.Location = new System.Drawing.Point(11, 14);
+            this.numericUpDownCount.Name = "numericUpDownCount";
+            this.numericUpDownCount.Size = new System.Drawing.Size(89, 26);
+            this.numericUpDownCount.TabIndex = 6;
+            this.numericUpDownCount.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // labelSum
+            // 
+            this.labelSum.AutoSize = true;
+            this.labelSum.Location = new System.Drawing.Point(100, 132);
+            this.labelSum.Name = "labelSum";
+            this.labelSum.Size = new System.Drawing.Size(45, 20);
+            this.labelSum.TabIndex = 7;
+            this.labelSum.Text = "label2";
+            // 
+            // dataGridViewSort
+            // 
+            this.dataGridViewSort.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewSort.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSort.Location = new System.Drawing.Point(11, 164);
+            this.dataGridViewSort.Name = "dataGridViewSort";
+            this.dataGridViewSort.Size = new System.Drawing.Size(480, 68);
+            this.dataGridViewSort.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 282);
+            this.Controls.Add(this.dataGridViewSort);
+            this.Controls.Add(this.labelSum);
+            this.Controls.Add(this.numericUpDownCount);
+            this.Controls.Add(this.dataGridViewTable);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridViewTable;
+        private System.Windows.Forms.NumericUpDown numericUpDownCount;
+        private System.Windows.Forms.Label labelSum;
+        private System.Windows.Forms.DataGridView dataGridViewSort;
+    }
+}
+
---
+++ Arrays1DWinForms/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays1DWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                int n;
+                if (int.TryParse(numericUpDownCount.Text, out n))
+                {
+                    try
+                    {
+                        dataGridViewTable.ColumnCount = n;
+                        dataGridViewTable.RowCount = 1;
+                        for (int i = 0; i < n; i++)
+                        {
+                            dataGridViewTable.Columns[i].HeaderText = i.ToString();
+                            dataGridViewTable.Columns[i].Width = 45;
+                        }
+                        Random rand = new Random();
+                        for (int i = 0; i < dataGridViewTable.ColumnCount; i++)
+                            dataGridViewTable[i, 0].Value = Math.Round(rand.NextDouble() * (16.3 + 12.3) - 12.3, 1);
+                       
+                        dataGridViewTable.ClearSelection();
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка", "Неправильное количество элементов");
+                    }
+                }
+                else
+                    MessageBox.Show("Ошибка", "Неправильное количество элементов");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxCount_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] array = new double[dataGridViewTable.ColumnCount];
+            int totalSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = double.Parse(dataGridViewTable[i, 0].Value.ToString());
+            }
+            if (array.Length == 0)
+            {
+                MessageBox.Show("Ошибка", "Неправильное количество элементов");
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] < 0 && i % 2 == 0)
+                    {
+                        totalSum += i;
+                    }
+                }
+                labelSum.Text = "Sum = " + totalSum.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double[] array = new double[dataGridViewTable.ColumnCount];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = double.Parse(dataGridViewTable[i, 0].Value.ToString());
+            }
+            int first = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    first = i;
+                    break;
+                }
+            }
+            int second = 0;
+            for (int i = first + 1; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    second = i;
+                    break;
+                }
+            }
+            dataGridViewSort.ColumnCount = array.Length;
+            dataGridViewSort.RowCount = 1;
+            double temp;
+
+            // traverse 0 to array length 
+            for (int i = first; i < second - 1; i++)
+
+                // traverse i+1 to array length 
+
+                    // compare array element with  
+                    // all next element 
+                    if (array[i] < array[i + 1])
+                    {
+
+                        temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+            for (int i = 0; i < array.Length; i++)
+            {
+                dataGridViewSort.Columns[i].HeaderText = i.ToString();
+                dataGridViewSort.Columns[i].Width = 45;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                dataGridViewSort[i, 0].Value = array[i];
+            }
+        }
+    }
+}
